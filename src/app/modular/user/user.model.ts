51cf466dabceb5a8db7898f1f3d0131d6b0856a9
jahid45780 <jahidhossain88899@gmail.com>
2026-04-17 23:39:14 +0600

    add the user model

--- src/app/modular/user/user.model.ts
+++ src/app/modular/user/user.model.ts
@@ -1,1 +1,39 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, isActive, IUser, Role } from "./user.interface";
 
+
+export const authProviderSchema = new Schema<IAuthProvider>({
+     provider:{type:String, required:true},
+     providerID:{type:String, required:true}
+},{
+    versionKey:false,
+    id:false
+})
+
+export const userSchema = new Schema<IUser>({
+
+    name:{type:String, required:true},
+    email:{type:String, required:true, unique:true},
+    password:{type:String},
+    phone:{type:String},
+    address:{type:String},
+    picture:{type:String},
+    role:{
+        type:String,
+        enum:Object.values(Role),
+        default:Role.USER
+    },
+    IsActive:{
+        type:String,
+        enum:Object.values(Role),
+        default:isActive.ACTIVE
+    },
+    IsDeleted:{type:Boolean, default:false},
+    IsVerified:{type:Boolean, default:false},
+    auths:[authProviderSchema]  
+},{
+    timestamps:true,
+    versionKey:false
+})
+
+export const User = model<IUser>("USER", userSchema)
