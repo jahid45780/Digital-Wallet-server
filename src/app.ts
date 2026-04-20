a693a5f67f97524f269382e7fe926d5213ce7e03
jahid45780 <jahidhossain88899@gmail.com>
2026-04-21 00:42:30 +0600

    passport config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 import express, { Request,  Response } from 'express'
+import cors from "cors"
 import { globalErrorHandler } from './app/middleware/globalErrorHandler'
 import NotFound from './app/middleware/NotFound'
 import { router } from './app/routes'
+import cookieParser from "cookie-parser";
+import expressSession from 'express-session'
+import { envVers } from './app/config/env'
+import passport from 'passport'
+
 
 const app = express()
 
+app.use(expressSession({
+    secret: envVers.EXPRESS_SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
 app.use(express.json());
-
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
+app.use(cors({
+  origin:envVers.FRONTEND_URL,
+  credentials:true
+}))
 
 app.use("/api/v1", router)
 
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,18 +1,30 @@
 import dotenv from "dotenv"
 
+
 dotenv.config()
 
 interface envConfig {
     PORT: string,
     DB_URL: string,
     NODE_ENV: "development" | "production",
-    BCRYPT_SALT_ROUND:string
+    JWT_ACCESS_SECRET:string,
+    JWT_ACCESS_EXPIRES:string,
+    JWT_ACCESS_REFRESH_SECRET:string,
+    JWT_ACCESS_REFRESH_EXPIRES:string,
+    BCRYPT_SALT_ROUND:string,
+    GOOGLE_CLIENT_ID:string,
+    GOOGLE_CLIENT_SECRET:string,
+    GOOGLE_CALLBACK_URL:string,
+    EXPRESS_SESSION_SECRET:string,
+    FRONTEND_URL:string
 }
 
 const loadEnvVars = ():envConfig=>{
     const reqEnvVars : string[] = [
         "PORT", "DB_URL",
-        "NODE_ENV","BCRYPT_SALT_ROUND"
+        "NODE_ENV","BCRYPT_SALT_ROUND","JWT_ACCESS_SECRET","JWT_ACCESS_EXPIRES",
+        "JWT_ACCESS_REFRESH_SECRET","JWT_ACCESS_REFRESH_EXPIRES","GOOGLE_CLIENT_ID",
+        "GOOGLE_CLIENT_SECRET","GOOGLE_CALLBACK_URL","EXPRESS_SESSION_SECRET","FRONTEND_URL"
     ]
 
      reqEnvVars.forEach(key=>{
@@ -25,8 +37,17 @@ const loadEnvVars = ():envConfig=>{
     PORT:process.env.PORT as string,    
     DB_URL: process.env.DB_URL as string,
     NODE_ENV:process.env.NODE_ENV as "development" | "production",
+    JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES:process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_ACCESS_REFRESH_SECRET:process.env.JWT_ACCESS_REFRESH_SECRET as string,
+    JWT_ACCESS_REFRESH_EXPIRES:process.env.JWT_ACCESS_REFRESH_EXPIRES as string,
+    BCRYPT_SALT_ROUND:process.env.BCRYPT_SALT_ROUND as string,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL as string,
+    EXPRESS_SESSION_SECRET:process.env.EXPRESS_SESSION_SECRET as string,
+    FRONTEND_URL:process.env.FRONTEND_URL as string
 
-    BCRYPT_SALT_ROUND:process.env.BCRYPT_SALT_ROUND as string
     }
 }
 
