4d55ff3d014f7c5c6cfb3c44a998c5c8861847c8
jahid45780 <jahidhossain88899@gmail.com>
2026-08-03 13:38:03 +0600

    add the sendemail

---
+++ src/app/utils/sendEmail.ts
@@ -0,0 +1,56 @@
+import nodemailer from "nodemailer";
+import { envVers } from "../config/env";
+import path from "path";
+import ejs from "ejs";
+import AppError from "../errorHerplrs/appError";
+
+const transporter = nodemailer.createTransport({
+    secure: true,
+    auth:{
+        user:envVers.EMAIL_SENDER.SMTP_USER,
+        pass:envVers.EMAIL_SENDER.SMTP_PASS
+    },
+    port: Number(envVers.EMAIL_SENDER.SMTP_PORT),
+    host: envVers.EMAIL_SENDER.SMTP_HOST
+})
+
+interface SendEmailOptions  {
+    to: string;
+    subject: string;
+    templateName: string;
+    templateData: Record<string, any>;
+    attachments?:{
+        filename: string;
+        content: Buffer | string;
+        contentType?: string;
+    }[]
+}
+
+export const sendEmail = async ({
+     to,
+     subject,
+     templateName,
+     templateData,
+     attachments
+}: SendEmailOptions) => {
+    try{
+       const templatePath = path.join(__dirname, `templates/${templateName}.ejs`);
+       const html = await ejs.renderFile(templatePath, templateData);
+       const info = await transporter.sendMail({
+          from: envVers.EMAIL_SENDER.SMTP_USER,
+          to: to,
+          subject: subject,
+          html: html,
+          attachments: attachments?.map(attachments =>({
+            filename:attachments.filename,
+            content:attachments.content,
+            contentType:attachments.contentType
+          }))
+       })
+       console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`); 
+
+    } catch(err:any){
+         console.log("email sending error", err.message);
+        throw new AppError(401, "Email error")
+    }
+}
